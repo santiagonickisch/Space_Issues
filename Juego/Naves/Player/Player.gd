d9142428fa4da3303d1b4997112107a821913174
santiago nickisch <SantiNick28@gmail.com>
2022-10-14 11:32:44 -0300

    Clase 4
    - Agregado disparo secundario desde la clonacion del laserbream, su input y respectivo script
    - Agregado Grow al disparo primario y secundario

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -12,17 +12,21 @@ var dir_rotacion:int = 0
 
 ## Atributos Onready
 onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
 
 ## Metodos
-# warning-ignore:unused_argument
+func _unhandled_input(event: InputEvent) -> void:
+	#Disparo Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+	
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	# Los print son solo para entender mejor que esta pasando. BORRAR
-	print(rad2deg(rotation))
-	print(empuje.rotated(rotation))
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
-# warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	player_input()
 
